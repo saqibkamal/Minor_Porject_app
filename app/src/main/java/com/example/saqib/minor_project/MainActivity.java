58de91e11cfe3bd775c0200640f49e7146d02c7f
saqibkamal <saqib.kamal01@gmail.com>
2018-08-28 14:43:07 +0530

    Base Model

---
+++ app/src/main/java/com/example/saqib/minor_project/MainActivity.java
@@ -0,0 +1,207 @@
+package com.example.saqib.minor_project;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+
+import android.os.StrictMode;
+import android.provider.MediaStore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Random;
+
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
+    ImageView imageView;
+
+    File imagefile;
+    Uri imageUri;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.INTERNET
+                ).withListener(new MultiplePermissionsListener() {
+            @Override public void onPermissionsChecked(MultiplePermissionsReport report) {/* ... */}
+            @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {/* ... */}
+        }).check();
+
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView)  findViewById(R.id.imageView);
+
+        button.setOnClickListener(this);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button){
+            Random rand = new Random();
+
+            int  n = rand.nextInt(500000) + 1;
+            Intent intent =  new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            imagefile = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES),"tesfsdft"+n+".jpg"
+            );
+            imageUri=Uri.fromFile(imagefile);
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY,1);
+
+            startActivityForResult(intent,0);
+
+
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==0){
+            switch (resultCode){
+                case Activity.RESULT_OK:
+                    Log.i("File","Sucess"+imagefile.getAbsolutePath());
+                    imageView.setImageURI(imageUri);
+
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                        sendPhoto(bitmap);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+                    break;
+                case Activity.RESULT_CANCELED:
+                    Log.i("File","Failed");
+                    break;
+                default:
+                    Log.i("File","Badly Failed");
+            }
+        }
+    }
+
+    private void sendPhoto(Bitmap bitmap) throws Exception {
+        new UploadTask().execute(bitmap);
+    }
+
+    private class UploadTask extends AsyncTask<Bitmap, Void, Void> {
+
+        protected Void doInBackground(Bitmap... bitmaps) {
+            if (bitmaps[0] == null)
+                return null;
+
+
+           String ImageString=encodeImage(bitmaps[0]);
+
+
+            JSONObject jsonObject=new JSONObject();
+            try {
+                jsonObject.put("image",ImageString);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            try {
+                HttpPost httppost = new HttpPost(
+                        "http://10.0.2.2:5000/predict"); // server
+
+                StringEntity se;
+                se = new StringEntity(jsonObject.toString());
+                httppost.setEntity(se);
+
+                httppost.setHeader("Content-type", "application/json");
+
+                HttpResponse response = null;
+                try {
+                    response = httpclient.execute(httppost);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    if (response != null)
+                        Log.i("TAG", "response " + response.getStatusLine().toString());
+                } finally {
+
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } finally {
+
+            }
+
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+            Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_LONG).show();
+        }
+    }
+    
+    public byte[] getBytesFromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+        return stream.toByteArray();
+    }
+    public String encodeImage(Bitmap imageByteArray) {
+        return android.util.Base64.encodeToString(getBytesFromBitmap(imageByteArray),
+                android.util.Base64.NO_WRAP);
+    }
+
+}
